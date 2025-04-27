2dfa02cd87e086efbe70d33679d0349a9c79baeb
smjest0802 <smjest0802@gmail.com>
2025-04-27 15:08:05 -0400

    Added clock and circle shape class

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
+
+        time_passed = clock.tick(60)
+        dt = time_passed / 1000 # convert from milliseconds to seconds
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
