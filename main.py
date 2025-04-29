5ac8c4cb8e5f3850d78839643ca1948357c95e01
smjest0802 <smjest0802@gmail.com>
2025-04-28 22:04:20 -0400

    Added ship

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pygame
 from constants import *
+import player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player1 = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player1.draw(screen)
+
         pygame.display.flip()
 
         time_passed = clock.tick(60)
